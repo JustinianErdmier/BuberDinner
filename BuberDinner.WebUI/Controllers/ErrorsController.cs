80d9181f37ef2f5b480e1a3e6bf7413c11a4177d
JustinErdmier <justin.erdmier@ltnow.com>
2022-11-17 14:43:46 -0600

    :four: global error handling

---
+++ BuberDinner.WebUI/Controllers/ErrorsController.cs
@@ -0,0 +1,22 @@
+﻿// ---------------------------------------------------------------------------------------------------------------------------------
+// Copyright (c) Lieberman Technologies, LLC. All rights reserved.
+// BuberDinner > BuberDinner.WebUI > ErrorsController.cs
+// Created: 11 11, 2022
+// Modified: 11 11, 2022
+// ---------------------------------------------------------------------------------------------------------------------------------
+
+using Microsoft.AspNetCore.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BuberDinner.WebUI.Controllers;
+
+public class ErrorsController : ControllerBase
+{
+    [Route("error")]
+    public IActionResult Error()
+    {
+        Exception? exception = HttpContext.Features.Get<IExceptionHandlerFeature>()?.Error;
+        
+        return Problem(title: exception?.Message);
+    }
+}
