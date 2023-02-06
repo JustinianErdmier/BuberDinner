064efea32c0e4b27b6494e569e6ae3511effdd22
JustinErdmier <justin.erdmier@outlook.com>
2023-02-05 21:43:43 -0600

    :twelve: implementing AggregateRoot, Entity, ValueObject

---
+++ BuberDinner.Domain/Common/Models/Entity.cs
@@ -0,0 +1,20 @@
+﻿namespace BuberDinner.Domain.Common.Models;
+
+public abstract class Entity<TId> : IEquatable<Entity<TId>>
+    where TId : notnull
+{
+    protected Entity(TId id) => Id = id;
+
+    public TId Id { get; }
+
+    public bool Equals(Entity<TId>? other) => Equals((object?) other);
+
+    public override bool Equals(object? obj) => obj is Entity<TId> entity && Id.Equals(entity.Id);
+
+    public static bool operator ==(Entity<TId> left, Entity<TId> right) => Equals(left, right);
+
+    public static bool operator !=(Entity<TId> left, Entity<TId> right) => ! Equals(left, right);
+
+    /// <inheritdoc />
+    public override int GetHashCode() => Id.GetHashCode();
+}
---
+++ BuberDinner.Domain/Common/Models/ValueObject.cs
@@ -0,0 +1,29 @@
+﻿namespace BuberDinner.Domain.Common.Models;
+
+public abstract class ValueObject : IEquatable<ValueObject>
+{
+    public bool Equals(ValueObject? other) => Equals((object?) other);
+
+    public abstract IEnumerable<object> GetEqualityComponents();
+
+    public override bool Equals(object? obj)
+    {
+        if (obj is null || obj.GetType() != GetType())
+            return false;
+
+        var other = (ValueObject) obj;
+
+        return GetEqualityComponents()
+            .SequenceEqual(other.GetEqualityComponents());
+    }
+
+    public static bool operator ==(ValueObject left, ValueObject right) => Equals(left, right);
+
+    public static bool operator !=(ValueObject left, ValueObject right) => ! Equals(left, right);
+
+    /// <inheritdoc />
+    public override int GetHashCode() =>
+        GetEqualityComponents()
+            .Select(x => x?.GetHashCode() ?? 0)
+            .Aggregate((x, y) => x ^ y);
+}
